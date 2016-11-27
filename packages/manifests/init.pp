c374c2be093114b98cfdaa9d4af3014092c730e4
Keone Martin <s3247904@student.rmit.edu.au>
2016-11-26 21:32:21 -0500

    added packages module to install packages and enable as service on boot

---
+++ packages/manifests/init.pp
@@ -0,0 +1,60 @@
+# Class: packages
+# ===========================
+#
+# Full description of class packages here.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'packages':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <author@domain.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2016 Your name here, unless otherwise noted.
+#
+class packages {
+
+  Package { ensure => 'present' }
+
+  Service { enable => true }
+
+  $required = [ 
+    'httpd', 'apache_server', 'openssh', 'MySQL Server', 'vncserver', 'tmux', 'dia2code', 'lynx', 'gcc', '    gdb', 'cgdb', 'vim', 'emacs', 'sshfs' 
+  ]
+
+  package { $required: }
+
+  service { $required: }
+
+  
+}
